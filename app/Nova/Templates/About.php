0b4d50e863f9e8bef6409d3e36ce0e8475dfc440
IKramooe <ikramatmanemi@gmail.com>
2024-02-04 14:25:59 +0100

    up

--- app/Nova/Templates/About.php
+++ app/Nova/Templates/About.php
@@ -3,6 +3,17 @@
 namespace App\Nova\Templates;
 
 use Illuminate\Http\Request;
+use Whitecube\NovaFlexibleContent\Flexible;
+use Mdixon18\Fontawesome\Fontawesome;
+
+use Laravel\Nova\Panel;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\Select;
+
+use Laravel\Nova\Fields\Image;
 use Whitecube\NovaPage\Pages\Template;
 
 class About extends Template {
